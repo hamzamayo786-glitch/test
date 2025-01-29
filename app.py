372b48f0303000410e4f38d95432ff8f3c0e85f2
hamzamayo786-glitch <hamzamayo408@gmail.com>
2025-01-29 22:35:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def fetch_video_data():
         return jsonify({"error": "Failed to fetch data from the external API.", "details": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
